24853d325291010bea45ecc9c111233ff208a2e6
valentinminkov <minkowalentin@proton.me>
2024-07-12 16:15:25 +0200

    feat: change darkmode to be a switch instead of dropdown because we will use only 2 elements.

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,13 +1,6 @@
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Toggle } from "@/components/ui/toggle";
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<"theme-light" | "dark">(
@@ -26,22 +19,12 @@ export function ModeToggle() {
   }, [theme]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Toggle
+      aria-label="Toggle dark mode"
+      onClick={() => setThemeState(theme === "dark" ? "theme-light" : "dark")}
+    >
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    </Toggle>
   );
 }
